3dbd186fb51826d573e9583397301064a4edea60
Charlie Huang <charliehuang09@gmail.com>
2026-09-05 12:03:11 -0700

    change bos to cos
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- src/control_loop/connect_to_rio.h
+++ src/control_loop/connect_to_rio.h
@@ -4,5 +4,5 @@ namespace control_loop {
 // Sets up logging and networktables. Should be called at the beggining of every robot's main
 void StartNetworktables(int team_number = 971);
 void StartNetworktablesAsHost();
-auto GetNewLogPath(const std::string& log_dir = "/bos/logs") -> std::string;
+auto GetNewLogPath(const std::string& log_dir = "/cos/logs") -> std::string;
 }  // namespace control_loop
